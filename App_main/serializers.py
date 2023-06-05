6ce04a28cda1e7831df32a4cb9f9971a5784a1e5
evana27perveen <evana27perveen2@gmail.com>
2023-06-06 01:39:26 +0600

    Added serializers and views

--- App_main/serializers.py
+++ App_main/serializers.py
@@ -1,1 +1,75 @@
 from rest_framework import serializers
+from .models import (
+    IncomeTaxReturn,
+    IncomeTaxRegistration,
+    SalesTaxRegistration,
+    TaxVerification,
+    TaxCalculation,
+    EPayment,
+    PaymentRecord,
+    PenaltyCalculation,
+)
+
+
+class IncomeTaxReturnSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = IncomeTaxReturn
+        fields = '__all__'
+
+
+class IncomeTaxRegistrationSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = IncomeTaxRegistration
+        fields = '__all__'
+
+
+class SalesTaxRegistrationSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = SalesTaxRegistration
+        fields = '__all__'
+
+
+class TaxVerificationSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = TaxVerification
+        fields = '__all__'
+
+
+class TaxCalculationSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = TaxCalculation
+        fields = '__all__'
+
+
+class EPaymentSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = EPayment
+        fields = '__all__'
+
+
+class PaymentRecordSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = PaymentRecord
+        fields = '__all__'
+
+
+class PenaltyCalculationSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = PenaltyCalculation
+        fields = '__all__'
--- App_main/views.py
+++ App_main/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import (
+    IncomeTaxRegistration,
+    SalesTaxRegistration,
+    TaxVerification,
+    TaxCalculation,
+    EPayment,
+    PaymentRecord,
+    PenaltyCalculation,
+)
+from .serializers import (
+    IncomeTaxRegistrationSerializer,
+    SalesTaxRegistrationSerializer,
+    TaxVerificationSerializer,
+    TaxCalculationSerializer,
+    EPaymentSerializer,
+    PaymentRecordSerializer,
+    PenaltyCalculationSerializer,
+)
 
-# Create your views here.
+
+class IncomeTaxRegistrationCreateView(APIView):
+    def post(self, request):
+        serializer = IncomeTaxRegistrationSerializer(data=request.data, context={'user': request.user})
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class IncomeTaxRegistrationListView(APIView):
+    def get(self, request):
+        queryset = IncomeTaxRegistration.objects.filter(user=request.user)
+        serializer = IncomeTaxRegistrationSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class IncomeTaxRegistrationDetailView(APIView):
+    def get_object(self, pk):
+        try:
+            return IncomeTaxRegistration.objects.get(pk=pk, user=self.request.user)
+        except IncomeTaxRegistration.DoesNotExist:
+            raise status.HTTP_404_NOT_FOUND
+
+    def get(self, request, pk):
+        instance = self.get_object(pk)
+        serializer = IncomeTaxRegistrationSerializer(instance)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        instance = self.get_object(pk)
+        serializer = IncomeTaxRegistrationSerializer(instance, data=request.data, context={'user': request.user})
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        instance = self.get_object(pk)
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# Similarly, define the remaining views for other models: SalesTaxRegistration, TaxVerification,
+# TaxCalculation, EPayment, PaymentRecord, and PenaltyCalculation.
+
+
